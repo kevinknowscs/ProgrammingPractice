b5c4e034ddd9a98bbab2622410dd0501326bb09f
Kevin Dietz <kevinknowscs@gmail.com>
2018-04-14 15:27:55 -0600

    Determine if a string is a Palindrome

---
+++ App.Problems..Palindrome/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace App.Problems.Palindrome
+{
+  class Program
+  {
+    static bool IsPalindrome(string s)
+    {
+      // A simple function to determine if an arbitrary string is a Palindrome.
+
+      if (s.Length <= 1)
+        return true;
+
+      for (int index = 0; index < s.Length / 2; index++)
+      {
+        if (s[index] != s[s.Length - 1 - index])
+          return false;
+      }
+
+      return true;
+    }
+
+    static void Main(string[] args)
+    {
+      string[] testWords = { "", "x", "kevin", "kayak", "bb", "ab", "abba", "aba", "bbc" };
+
+      foreach (string word in testWords)
+      {
+        Console.WriteLine("Word: '{0}', IsPalindrome: {1}", word, IsPalindrome(word));
+      }
+    }
+  }
+}
