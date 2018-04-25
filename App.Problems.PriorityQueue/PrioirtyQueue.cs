9de79e990f6705cdb9992b7226110aefdb18e214
Kevin Dietz <kevinknowscs@gmail.com>
2018-04-25 11:20:36 -0600

    Priority Queue

---
+++ App.Problems.PriorityQueue/PrioirtyQueue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace App.Problems.PriorityQueue
+{
+  public class PriorityQueue<T>
+  {
+    public PriorityQueue()
+    {
+      Items = new SortedDictionary<int, Queue<T>>();
+      Count = 0;
+    }
+
+    private SortedDictionary<int, Queue<T>> Items { get; set; }
+
+    public int Count { get; private set; }
+
+    public bool IsEmpty => Count == 0;
+
+    public PriorityQueue<T> Enqueue(int priority, T item)
+    {
+      Queue<T> queue = null;
+
+      if (Items.ContainsKey(priority))
+      {
+        queue = Items[priority];
+      }
+      else
+      {
+        queue = new Queue<T>();
+        Items.Add(priority, queue);
+      }
+
+      queue.Enqueue(item);
+      Count++;
+
+      return this;
+    }
+
+    public T Dequeue()
+    {
+      if (Count == 0)
+        throw new Exception("Queue is empty");
+
+      var firstPriority = Items.First();
+      var queue = firstPriority.Value;
+
+      T item = queue.Dequeue();
+      Count--;
+
+      if (queue.Count == 0)
+        Items.Remove(firstPriority.Key);
+
+      return item;
+    }
+  }
+}
---
+++ App.Problems.PriorityQueue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Problems.PriorityQueue
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var priorityQueue = new PriorityQueue<string>();
+
+      priorityQueue.Enqueue(100, "Lowest prioirty item");
+      priorityQueue.Enqueue(1, "A high priority item");
+      priorityQueue.Enqueue(1, "Another high priority item");
+      priorityQueue.Enqueue(10, "A middle priority item");
+
+      while (priorityQueue.Count != 0)
+      {
+        var item = priorityQueue.Dequeue();
+        Console.WriteLine(item);
+      }
+    }
+  }
+}
