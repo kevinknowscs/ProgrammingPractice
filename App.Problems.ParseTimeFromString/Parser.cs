c76f049f2106b552939c5776e5dd228c2d9bae50
Kevin Dietz <kevinknowscs@gmail.com>
2018-04-25 22:19:20 -0600

    Parse Time From String

---
+++ App.Problems.ParseTimeFromString/Parser.cs
@@ -0,0 +1,96 @@
+﻿
+namespace App.Problems.ParseTimeFromString
+{
+  public class Parser
+  {
+    private const string RetriesLabel = "Retries";
+    private const string TotalTimeLabel = "TotalTime";
+
+    private int SkipChar(string val, int pos, char ch)
+    {
+      while (pos < val.Length && val[pos] == ch)
+        pos++;
+
+      return pos;
+    }
+
+    private int SkipColon(string val, int pos)
+    {
+      return SkipChar(val, pos, ':');
+    }
+
+    private int SkipSemicolon(string val, int pos)
+    {
+      return SkipChar(val, pos, ';');
+    }
+
+    private int SkipSpaces(string val, int pos)
+    {
+      return SkipChar(val, pos, ' ');
+    }
+
+    private bool IsDigit(char ch)
+    {
+      return ch >= '0' && ch <= '9';
+    }
+
+    private int GetIntFromChar(char ch)
+    {
+      return (int)(ch - '0');
+    }
+
+    private int GetNextValue(string val, int pos, out int result)
+    {
+      result = 0;
+
+      while (pos < val.Length && IsDigit(val[pos]))
+        result = result * 10 + GetIntFromChar(val[pos++]);
+
+      pos = SkipSpaces(val, pos);
+      pos = SkipSemicolon(val, pos);
+      pos = SkipSpaces(val, pos);
+
+      return pos;
+    }
+
+    private int GetRetries(string val, int pos, out int result)
+    {
+      result = 0;
+
+      pos += RetriesLabel.Length;
+      pos = SkipColon(val, pos);
+      pos = SkipSpaces(val, pos);
+
+      return GetNextValue(val, pos, out result);
+    }
+
+    private int GetTotalTime(string val, int pos, out int result)
+    {
+      result = 0;
+
+      pos += TotalTimeLabel.Length;
+      pos = SkipColon(val, pos);
+      pos = SkipSpaces(val, pos);
+
+      return GetNextValue(val, pos, out result);
+    }
+
+    public int GetTotalTimeSum(string val)
+    {
+      int pos = 0;
+      int retries = 0;
+      int totalTime = 0;
+      int sum = 0;
+
+      pos = GetRetries(val, pos, out retries);
+
+      for (int x=0; x < retries + 1; x++)
+      {
+        pos = GetTotalTime(val, pos, out totalTime);
+        sum += totalTime;
+      }
+
+      return sum;
+    }
+  }
+}
---
+++ App.Problems.ParseTimeFromString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace App.Problems.ParseTimeFromString
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Given a string of the form "Retries: 2; TotalTime: 10000; TotalTime: 5000; TotalTime: 2000", write
+      // a function that returns  the total time
+      //
+      // The number of TotalTime occurrences is 1 + the number of Retries.
+      //
+      // DO NOT use regular expressions or any higher-level existing methods such as .NET's built-in
+      // Int32.Parse() or Int32.TryParse().
+
+      var parser = new Parser();
+
+      int sum = parser.GetTotalTimeSum("Retries: 2; TotalTime: 10000; TotalTime: 5000; TotalTime: 2000");
+
+      Console.WriteLine(sum);
+    }
+  }
+}
