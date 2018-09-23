cf4ee2bb52f00b755e26cbbdc1208a20d37bcd04
Kevin Dietz <kevinknowscs@gmail.com>
2018-09-23 00:12:24 -0600

    Snakes and Ladders Solution

---
+++ App.Problems.SnakesAndLadders/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace App.Problems.SnakesAndLadders
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var input = new int[,]
+      {
+        { -1, -1, -1, -1, -1, -1 },
+        { -1, -1, -1, -1, -1, -1 },
+        { -1, -1, -1, -1, -1, -1 },
+        { -1, 35, -1, -1, 13, -1 },
+        { -1, -1, -1, -1, -1, -1 },
+        { -1, 15, -1, -1, -1, -1 }
+      };
+
+      var board = new SnakesAndLaddersBoard(6, input);
+
+      Console.WriteLine(board.Solve());
+    }
+  }
+}
---
+++ App.Problems.SnakesAndLadders/SnakesAndLaddersBoard.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Problems.SnakesAndLadders
+{
+  public class SnakesAndLaddersBoard
+  {
+    public SnakesAndLaddersBoard(int size, int [,] input)
+    {
+      Size = size;
+      Input = input;
+    }
+
+    public int Size { get; }
+
+    public int[,] Input { get; }
+
+    private int GetRow(int cellNo)
+    {
+      return Size - (cellNo - 1) / Size - 1;
+    }
+
+    private int GetColumn(int cellNo)
+    {
+      int rowOffset = (cellNo - 1) / Size;
+      int offset = (cellNo - 1) % Size;
+
+      return (rowOffset % 2 == 0) ? offset : Size - offset - 1;
+    }
+
+    private int GetCellNo(int row, int col)
+    {
+      return 0;
+    }
+
+    private Dictionary<int, List<int>> ScanCells()
+    {
+      var result = new Dictionary<int, List<int>>();
+
+      for (int cellNo=1; cellNo <= Size * Size; cellNo++)
+      {
+        int row = GetRow(cellNo);
+        int col = GetColumn(cellNo);
+
+        if (Input[row, col] != -1)
+        {
+          int indirectCellNo = Input[row, col];
+          List<int> list = null;
+
+          if (result.ContainsKey(indirectCellNo))
+            list = result[indirectCellNo];
+          else
+          {
+            list = new List<int>();
+            result.Add(indirectCellNo, list);
+          }
+
+          list.Add(cellNo);
+        }
+      }
+
+      return result;
+    }
+
+    public int Solve(Dictionary<int, List<int>> scanned, HashSet<int> currentSearch, Dictionary<int, int> saved, int cellNo)
+    {
+      if (saved.ContainsKey(cellNo))
+        return saved[cellNo];
+
+      if (cellNo == 1)
+      {
+        saved.Add(1, 0);
+        return 0;
+      }
+
+      int min = Int32.MaxValue;
+
+      if (scanned.ContainsKey(cellNo))
+      {
+        foreach (int indirectCellNo in scanned[cellNo])
+        {
+          if (!currentSearch.Contains(indirectCellNo))
+          {
+            currentSearch.Add(indirectCellNo);
+            int currResult = Solve(scanned, currentSearch, saved, indirectCellNo);
+            currentSearch.Remove(indirectCellNo);
+
+            if (currResult < min)
+              min = currResult;
+          }
+        }
+      }
+
+      for (int prevCellNo=cellNo-1; prevCellNo > cellNo - 1 - Size && prevCellNo > 0; prevCellNo--)
+      {
+        int currRow = GetRow(prevCellNo);
+        int currColumn = GetColumn(prevCellNo);
+
+        if (Input[currRow, currColumn] == -1)
+        {
+          if (!currentSearch.Contains(prevCellNo))
+          {
+            currentSearch.Add(prevCellNo);
+            int currResult = Solve(scanned, currentSearch, saved, prevCellNo) + 1;
+            currentSearch.Remove(prevCellNo);
+
+            if (currResult < min)
+              min = currResult;
+          }
+        }
+      }
+
+      saved.Add(cellNo, min);
+
+      return min;
+    }
+
+    public int Solve()
+    {
+      var currentSearch = new HashSet<int>();
+      currentSearch.Add(Size * Size);
+
+      return Solve(ScanCells(), currentSearch, new Dictionary<int, int>(), Size * Size);
+    }
+  }
+}
